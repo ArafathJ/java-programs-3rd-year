1001c5fe2ae6ef9754eaaa92ad983dad4e746a59
Arafath.J <97463454+ArafathJ@users.noreply.github.com>
2026-03-01 10:58:02 +0530

    Change findFactorial to static method

--- P1/Factorial.java
+++ P1/Factorial.java
@@ -3,7 +3,7 @@ package P1;
 public class Factorial {
 
     // method to calculate factorial
-    public long findFactorial(int n) {
+    public static long findFactorial(int n) {
         long fact = 1;
 
         for (int i = 1; i <= n; i++) fact = fact * i;
